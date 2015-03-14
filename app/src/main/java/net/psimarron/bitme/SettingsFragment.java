27e550c6b6056d927f16d70f85d18fdfe608ecb5
JMS-1 <dev-2@psimarron.net>
2015-03-14 17:12:01 +0100

    Preferences dialog should now be complete - settings not yet used

--- app/src/main/java/net/psimarron/bitme/SettingsFragment.java
+++ app/src/main/java/net/psimarron/bitme/SettingsFragment.java
@@ -1,16 +1,36 @@
 package net.psimarron.bitme;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 // Pflegt die Einstellungen der Anwendung.
-public class SettingsFragment extends PreferenceFragment {
-
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Konfiguration der Einstellungen laden
         addPreferencesFromResource(R.xml.preferences);
+
+        // Änderungen aktualisieren die Anzeige
+        PreferenceManager.getDefaultSharedPreferences(getActivity()).registerOnSharedPreferenceChangeListener(this);
+
+        // Beschreibungen setzen
+        updateSummaries();
+    }
+
+    // Aktualisiert alle Beschreibungen.
+    public void updateSummaries() {
+        // Beschreibungen setzen
+        ListPreference level = (ListPreference) findPreference(getResources().getString(R.string.pref_strength_key));
+        level.setSummary(level.getEntry());
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        updateSummaries();
     }
 }
